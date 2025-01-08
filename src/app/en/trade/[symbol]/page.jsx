07a29592267d2383689b36fce1461837d3345e5f
tuosm9390 <tuosm123@naver.com>
2025-01-08 18:55:21 +0900

    page defaultSymbol 수정

--- src/app/en/trade/[symbol]/page.jsx
+++ src/app/en/trade/[symbol]/page.jsx
@@ -7,12 +7,11 @@ import Market from "../../../component/Market/Market";
 import Trades from "../../../component/Trades/Trades";
 import OrderForm from "../../../component/OrderForm/OrderForm";
 import MarketActivity from "../../../component/MarketActivity/MarketActivity";
-import { defaultSymbolState } from "../../../hooks/stateManagement";
 import BasicTable from "../../../component/BasicTable/BasicTable";
-import { useRecoilValue } from "recoil";
+import { useSymbolStore } from "../../../hooks/stateManagement";
 
 function page() {
-  const defaultSymbol = "BTCUSDT";
+  const { defaultSymbol } = useSymbolStore();
 
   return (
     <div className="p-8 grid grid-cols-[minmax(253px,320px)_minmax(510px,880px)_minmax(253px,320px)] grid-rows-[56px_360px_160px_minmax(169px,1fr)_minmax(146px,auto)_560px_24px] gap-1 sm:p-10 sm:pt-1">
