4305318dda2340e6f98b7feb578b07286401e7ed
tuosm9390 <tuosm123@naver.com>
2025-01-09 15:17:10 +0900

    require('tailwind-scrollbar-hide'),

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    './component/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./component/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('tailwind-scrollbar-hide'),
-  ],
-} 
+  plugins: [require("tailwind-scrollbar-hide")],
+};
